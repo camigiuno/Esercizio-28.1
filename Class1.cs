c44199bcca64c556bb1fa7e20399eac70b4d557b
paolo <paolo@LAPTOP-PFR3VSID>
2023-12-06 16:02:27 +0100

    Aggiungere i file di progetto.

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_28_1
+{
+    internal class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public DateTime DataDiNascita { get; set; }
+    }
+}
---
+++ Class3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_28_1
+{
+    internal class Voti
+    {
+        public DateTime Data {  get; set; }
+        public int Valore { get; set; } 
+
+    }
+}
